d765b16b3c37e2fe0af632180636c7839047970f
Riccardo2050 <riky.scola@outlook.it>
2025-09-23 16:55:41 +0200

    creation of java project
    we just build the maven progect and we add log4j-API and log4j-Core

---
+++ gestione-dipendenti-azienda/src/main/java/it/unibg/progetto/gestione_dipendenti_azienda/App.java
@@ -0,0 +1,10 @@
+package it.unibg.progetto.gestione_dipendenti_azienda;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ gestione-dipendenti-azienda/src/test/java/it/unibg/progetto/gestione_dipendenti_azienda/AppTest.java
@@ -0,0 +1,19 @@
+package it.unibg.progetto.gestione_dipendenti_azienda;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

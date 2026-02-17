178595e06641206b8e4b06ab77a0cca43d787a38
Riccardo2050 <104273564+RiccardoSC2050@users.noreply.github.com>
2026-02-17 19:28:00 +0100

    non va localhost
    da sistemare

---
+++ api/src/main/java/it/unibg/progetto/api/app/LauncherMain.java
@@ -0,0 +1,28 @@
+package it.unibg.progetto.api.app;
+
+
+
+import it.unibg.progetto.api.cli.components.GlobalScanner;
+
+public class LauncherMain {
+
+    public static void main(String[] args) {
+
+        
+
+        System.out.println("Seleziona modalità di avvio:");
+        System.out.println("1 - Grafica");
+        System.out.println("2 - Terminale");
+        System.out.print("Scelta: ");
+
+        String scelta = GlobalScanner.scanner.nextLine();
+
+        if (scelta.equals("1")) {
+            GuiMain.main(args);
+        } else if (scelta.equals("2")) {
+            ApiMain.main(new String[] { "--spring.main.web-application-type=none" });
+        } else {
+            System.out.println("Scelta non valida.");
+        }
+    }
+}
